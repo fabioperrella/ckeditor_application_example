3e69d7c8b22403dab5ab6580cdb2d3bb0b82a56c
Fabio Perrella <saas-dev@locaweb.com.br>
2012-05-24 14:37:26 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CkeditorTest::Application.config.secret_token = 'c03efaa0189addac3d5882f3b8e0eb5b23151745894466a8211523eedc08b645f8675268ab0a627d050069fca39cf6e01df8f686075b6c5bf6b57b66b2ffbccd'
